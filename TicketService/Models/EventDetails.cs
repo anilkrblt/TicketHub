50b7dd3fddd25d81686b7b403e74b8b9546d01a1
Anıl karabulut <darkside_of_the_software@hotmail.com>
2025-04-07 19:13:23 +0300

    TicketHub project start

---
+++ TicketService/Models/EventDetails.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketService.Models
+{
+    public class EventDetails
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string? Name { get; set; }
+        public string? Organizer { get; set; }
+        public string? Location { get; set; }
+        public DateTime EventDate { get; set; }
+        public required ICollection<Ticket> Tickets { get; set; }
+
+    }
+}
---
+++ TicketService/Models/Ticket.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketService.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int TicketId { get; set; }
+        public decimal Price { get; set; }
+        public string? SeatNumber { get; set; }
+
+        public bool IsSold { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey(nameof(EventDetails))]
+        public int EventId { get; set; }
+
+        public required EventDetails EventDetails { get; set; }
+
+        // event_id INT -- refers to Event_Details.id
+
+    }
+}
